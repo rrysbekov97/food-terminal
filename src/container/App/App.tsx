938780f02dac41866c6fa2c4c34d5d9e6a92ef80
rrysbekov <rysbekovrenat322@gmail.com>
2024-09-28 18:54:42 +0600

    third task code

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import {useState} from 'react';
 import FastFoodItem from '../../components/FastFoodItem/FastFoodItem';
-// import {fastfooditems} from './constants';
 import removeItemImg from '../../assets/remove.png';
 
 import {FastfoodItem} from '../../type';
