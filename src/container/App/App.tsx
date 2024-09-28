1e3a3c69134e88454d18004ab58563e417f01510
rrysbekov <rysbekovrenat322@gmail.com>
2024-09-28 18:53:04 +0600

    my second task commit

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,8 +1,25 @@
 import './App.css';
 import {useState} from 'react';
 import FastFoodItem from '../../components/FastFoodItem/FastFoodItem';
-import {fastfooditems} from './constants';
+// import {fastfooditems} from './constants';
 import removeItemImg from '../../assets/remove.png';
+
+import {FastfoodItem} from '../../type';
+import burgerLogo from '../../assets/burger.png';
+import friedLogo from '../../assets/fried.png';
+import coffeeLogo from '../../assets/coffee.png';
+import teaLogo from '../../assets/tea.png';
+import colaLogo from '../../assets/cola.png';
+
+ 
+export const fastfooditems:FastfoodItem[] = [
+  {name: 'Hamburger', price: 80, image: burgerLogo},
+  {name: 'Cheeseburger', price: 90, image: burgerLogo},
+  {name: 'Fries', price: 45, image: friedLogo},
+  {name: 'Coffee', price: 70, image: coffeeLogo},
+  {name: 'Tea', price: 50, image: teaLogo},
+  {name: 'Cola', price: 40, image: colaLogo}
+];
 
 const App = () => {
   const [order, setOrder] = useState([
